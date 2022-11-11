8a0f8c6d90063e761b43c4edaa39f64add124583
Tim <timwagner997@gmail.com>
2022-11-12 00:49:32 +0100

    Updated My Todos Screen
    added another elements, including a list of dummy todo elemets

--- lib/screens/my_todos_screen.dart
+++ lib/screens/my_todos_screen.dart
@@ -6,6 +6,13 @@ class MyTodosScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> dummyTodos = [
+      "Go out with the dog",
+      "Clean the kitchen",
+      "Buy cornflakes",
+      "Play world of warcraft",
+    ];
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.brown,
@@ -15,8 +22,9 @@ class MyTodosScreen extends StatelessWidget {
         ),
         onPressed: () {},
       ),
-      backgroundColor: Colors.brown.shade200,
+      backgroundColor: Colors.brown.shade100,
       body: SafeArea(
+        bottom: false,
         child: Column(
           children: [
             Center(
@@ -27,12 +35,55 @@ class MyTodosScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text(
-              "MooTODO",
-              style: TextStyle(
-                color: Colors.brown,
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Moo",
+                  style: TextStyle(color: Colors.brown, fontSize: 50),
+                ),
+                Text(
+                  "TODO",
+                  style: TextStyle(
+                    color: Colors.brown,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.brown.shade200,
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(30),
+                  ),
+                ),
+                child: ListView.builder(
+                  itemCount: dummyTodos.length,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (newValue) {},
+                        ),
+                        Text(
+                          dummyTodos[index],
+                          style: TextStyle(
+                            color: Colors.brown.shade800,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
           ],
