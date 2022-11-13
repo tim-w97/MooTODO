1f5195a9a4f79fb8e29e24a0772b1b8228746794
Tim <timwagner997@gmail.com>
2022-11-13 23:07:22 +0100

    changed delete feature
    the user now has to long press the todo item to delete it

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -16,35 +16,31 @@ class TodoItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<TodosProvider>(
       builder: (context, todosProvider, child) {
-        return Row(
-          children: [
-            Text(
-              todo.text,
-              style: TextStyle(
-                fontSize: 20,
-                color: todo.isDone ? Colors.brown.shade400 : Colors.brown,
-                decoration: todo.isDone ? TextDecoration.lineThrough : null,
+        return GestureDetector(
+          onLongPress: () {
+            todosProvider.removeTodo(todo);
+          },
+          child: Row(
+            children: [
+              Text(
+                todo.text,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: todo.isDone ? Colors.brown.shade400 : Colors.brown,
+                  decoration: todo.isDone ? TextDecoration.lineThrough : null,
+                ),
               ),
-            ),
-            const Spacer(),
-            TodoCheckbox(
-              value: todo.isDone,
-              onChanged: (newValue) {
-                if (newValue == null) return;
+              const Spacer(),
+              TodoCheckbox(
+                value: todo.isDone,
+                onChanged: (newValue) {
+                  if (newValue == null) return;
 
-                todosProvider.setDoneStatus(of: todo, to: newValue);
-              },
-            ),
-            IconButton(
-              onPressed: () {
-                todosProvider.removeTodo(todo);
-              },
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.brown,
+                  todosProvider.setDoneStatus(of: todo, to: newValue);
+                },
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
